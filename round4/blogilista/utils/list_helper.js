0cb61431c7aae252601e176f12196125bd65fd0f
Jiro Pitkänen <jiro.pitkanen@flovv.io>
2020-09-18 17:15:28 +0300

    assignments to 4.5

--- round4/blogilista/utils/list_helper.js
+++ round4/blogilista/utils/list_helper.js
@@ -2,4 +2,17 @@ const dummy = (blogs) => {
   return 1;
 };
 
-module.exports = { dummy };
+const totalLikes = (blogs) => {
+  return blogs.length ? blogs.reduce((sum, blog) => sum + blog.likes, 0) : 0;
+};
+
+const favoriteBlog = (blogs) => {
+  return blogs.length
+    ? blogs.reduce(
+        (prev, current) => (prev.likes > current.likes ? prev : current),
+        { likes: -1 }
+      )
+    : null;
+};
+
+module.exports = { dummy, totalLikes, favoriteBlog };

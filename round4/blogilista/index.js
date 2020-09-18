5db63c68ba68efb10786fa8c635c6e4d711c2304
Jiro Pitkänen <jiro.pitkanen@flovv.io>
2020-09-18 16:25:35 +0300

    blogs controller done

--- round4/blogilista/index.js
+++ round4/blogilista/index.js
@@ -3,19 +3,11 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const config = require('./utils/config');
+const blogsRouter = require('./controllers/blogs');
 
 const app = express();
 
-// const blogSchema = mongoose.Schema({
-//   title: String,
-//   author: String,
-//   url: String,
-//   likes: Number,
-// });
-
-// const Blog = mongoose.model('Blog', blogSchema);
-
-const Blog = require('./models/blog');
+// const Blog = require('./models/blog');
 
 mongoose.connect(config.MONGODB_URI, {
   useNewUrlParser: true,
@@ -25,19 +17,21 @@ mongoose.connect(config.MONGODB_URI, {
 app.use(cors());
 app.use(express.json());
 
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
+// app.get('/api/blogs', (request, response) => {
+//   Blog.find({}).then((blogs) => {
+//     response.json(blogs);
+//   });
+// });
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body);
+// app.post('/api/blogs', (request, response) => {
+//   const blog = new Blog(request.body);
 
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
+//   blog.save().then((result) => {
+//     response.status(201).json(result);
+//   });
+// });
+
+app.use('/api/blogs', blogsRouter);
 
 app.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`);

19dd08a7960f030b8aeb3120235135834f42a32b
Jiro Pitkänen <jiro.pitkanen@flovv.io>
2020-10-09 14:27:14 +0300

    assignments up to 6.8

---
+++ round6/redux-anecdotes/src/components/AnecdoteList.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { voteForAnecdote } from "../reducers/anecdoteReducer";
+
+const AnecdoteList = () => {
+  const anecdotes = useSelector((state) =>
+    state.sort((a, b) => b.votes - a.votes)
+  );
+  const dispatch = useDispatch();
+
+  const vote = (id) => {
+    dispatch(voteForAnecdote(id));
+    console.log("vote", id);
+  };
+
+  return (
+    <>
+      {anecdotes.map((anecdote) => (
+        <div key={anecdote.id}>
+          <div>{anecdote.content}</div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default AnecdoteList;

f2295cd83d139a1e850f6d7f89d60541ccd786b3
Jiro Pitkänen <jiro.pitkanen@flovv.io>
2020-10-11 13:44:38 +0300

    some more styling

--- round7/bloglist/src/components/Menu.js
+++ round7/bloglist/src/components/Menu.js
@@ -1,13 +1,21 @@
+import { AppBar, Toolbar, Button } from '@material-ui/core'
 import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Menu = ({ user, handleLogout }) => {
   return (
-    <div>
-      <Link to="/">blogs</Link>
-      <Link to="/users"> users </Link>
-      {user.name} logged in <button onClick={handleLogout}>logout</button>
-    </div>
+    <AppBar position="static">
+      <Toolbar>
+        <Button color="inherit" component={Link} to="/">
+          blogs
+        </Button>
+        <Button color="inherit" component={Link} to="/users">Users</Button>
+        {user.name} logged in
+        <Button color="inherit" onClick={handleLogout}>
+          logout
+        </Button>
+      </Toolbar>
+    </AppBar>
   )
 }
 

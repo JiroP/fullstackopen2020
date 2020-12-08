a15e57275c764fbfcda77a0f463783608497decd
Jiro Pitkänen <jiro.pitkanen@flovv.io>
2020-12-08 12:05:24 +0200

    8.7

--- round8/library/library-backend.js
+++ round8/library/library-backend.js
@@ -115,6 +115,10 @@ const typeDefs = gql`
       published: Int!
       genres: [String!]!
     ): Book
+    editAuthor(
+      name: String!
+      setBornTo: Int!
+    ): Author
   }
 `
 
@@ -142,6 +146,16 @@ const resolvers = {
       const book = { ...args, id: uuid() }
       books = books.concat(book)
       return book
+    },
+    editAuthor: (root, args) => {
+      const author = authors.find(({name}) => name === args.name)
+      
+      if (!author) {
+        return null;
+      }
+      const updatedAuthor = { ...author, born: args.setBornTo }
+      authors = authors.map(a => a.name === args.name ? updatedAuthor : a)
+      return updatedAuthor
     }
   }
 }

3f4585097a05a98610d6b9102562c41ddc08f031
Jiro Pitkänen <jiro.pitkanen@flovv.io>
2020-12-08 13:28:58 +0200

    8.9

--- round8/library-frontend/src/components/Books.js
+++ round8/library-frontend/src/components/Books.js
@@ -1,12 +1,24 @@
-import React from 'react'
+import { useQuery } from "@apollo/client";
+import React, { useEffect, useState } from "react";
+import { ALL_BOOKS } from "../queries";
 
 const Books = (props) => {
+  const result = useQuery(ALL_BOOKS);
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    if (result.data) {
+      setBooks(result.data.allBooks);
+    }
+  }, [result]);
 
   if (!props.show) {
-    return null
+    return null;
   }
 
-  const books = []
+  if (result.loading) {
+    return <div>loading...</div>;
+  }
 
   return (
     <div>
@@ -16,24 +28,20 @@ const Books = (props) => {
         <tbody>
           <tr>
             <th></th>
-            <th>
-              author
-            </th>
-            <th>
-              published
-            </th>
+            <th>author</th>
+            <th>published</th>
           </tr>
-          {books.map(a =>
+          {books.map((a) => (
             <tr key={a.title}>
               <td>{a.title}</td>
               <td>{a.author}</td>
               <td>{a.published}</td>
             </tr>
-          )}
+          ))}
         </tbody>
       </table>
     </div>
-  )
-}
+  );
+};
 
-export default Books
+export default Books;

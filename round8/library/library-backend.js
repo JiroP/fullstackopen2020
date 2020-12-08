69610445a6816262e228319137821cf9dbee347d
Jiro Pitkänen <jiro.pitkanen@flovv.io>
2020-12-08 11:33:04 +0200

    8.3

--- round8/library/library-backend.js
+++ round8/library/library-backend.js
@@ -92,10 +92,17 @@ const typeDefs = gql`
     genres: [String!]!
   }
 
+  type Author {
+    name: String!
+    id: ID!
+    bookCount: Int!
+  }
+
   type Query {
     bookCount: Int!
     authorCount: Int!
     allBooks: [Book!]!
+    allAuthors: [Author!]!
   }
 `
 
@@ -103,7 +110,11 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books
+    allBooks: () => books,
+    allAuthors: () => authors
+  },
+  Author: {
+    bookCount: (root) => books.filter(({author}) => author === root.name).length
   }
 }
 
